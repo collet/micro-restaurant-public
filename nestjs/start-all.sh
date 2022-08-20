d93b8e10c6851021e238d0bce91f88870af275df
Christian Brel <ch.brel@gmail.com>
2022-08-20 02:50:45 +0200

    - Updates to fix issue with start-all.sh script.

--- nestjs/start-all.sh
+++ nestjs/start-all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "starting all"
-docker-compose --file menu-service/docker-compose-menu.yml \
+docker-compose --env-file ./.env.docker \
+               --file menu-service/docker-compose-menu.yml \
                --file dining-service/docker-compose-dining.yml \
                --file kitchen-service/docker-compose-kitchen.yml up
