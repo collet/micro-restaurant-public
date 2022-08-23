7a84100f1c7d9b6015965811adb640070743e2fb
Philippe Collet <Philippe.Collet@unice.fr>
2022-08-23 17:12:39 +0200

    fix stopping script message

--- spring/stop-all.sh
+++ spring/stop-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "starting all"
+echo "stopping all"
 docker-compose --file menu-service/docker-compose-menu.yml \
                --file dining-service/docker-compose-dining.yml \
                --file kitchen-service/docker-compose-kitchen.yml down
